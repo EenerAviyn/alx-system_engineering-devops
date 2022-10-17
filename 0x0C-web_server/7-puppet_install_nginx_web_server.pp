da6a3b9bb461cb6ff155ed332d0a4e22f345569e
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-10-17 22:26:23 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,7 +2,7 @@
 
 exec {'/usr/bin/env apt-get -y update': }
 exec {'/usr/bin/env apt-get -y install nginx': }
-exec {'/usr/bin/env echo "Hellow World" > /var/www/html/index.nginx-debian.html': }
+exec {'/usr/bin/env echo "Hello World" > /var/www/html/index.nginx-debian.html': }
 exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default': }
 exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
 exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
